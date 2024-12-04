07035cdf1b7dcee2c18308e6daf959c867e9ff12
Patrick Encinares <patrickjoseph.greenapple@gmail.com>
2024-12-04 21:14:14 +0800

    FEAT : TRANSACTIONS

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'Inventory System',
+    'title' => 'CBeauty Lounge',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => 'Inventory System',
+    'logo' => 'CBeauty Lounge',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
---
+++ database/migrations/2024_12_04_124525_create_transaction_headers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionHeadersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_headers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('payment_method_id')->constrained('payment_methods')->onUpdate('cascade')->onDelete('restrict');
+            $table->text('address');
+            $table->decimal('total_amount', 13, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_headers');
+    }
+}
